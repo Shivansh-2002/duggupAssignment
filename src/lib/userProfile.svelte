d59f7e33b9306c9e313116d2fbcc4930f72f3175
Shivansh <shivanshj2002@gmail.com>
2024-03-10 04:02:07 +0530

    Done....

--- src/lib/userProfile.svelte
+++ src/lib/userProfile.svelte
@@ -1,38 +1,39 @@
 <script lang="ts">
+  import type { UserData } from "../routes/+page.svelte";
   import { Button } from "./components/ui/button";
-  export let userName: string;
+  export let userData: UserData;
 </script>
 
-<div class="mx-36 py-2 mt-10 flex justify-center">
+<div class="mx-40 py-2 mt-20 flex justify-between">
     <div>
         <div class="w-32">
             <img class="rounded-full border-[5px]border-[#EAEFF5]" src="/userImage.svg" alt="userImage" width="120" height="120">
         </div>
         <div class="text-3xl w-32 text-center justify-center font-bold text-[#4B5C6D]">
-            {userName}
+            {userData.userName}
         </div>
     </div>
 
     <div class="flex flex-col mx-10 my-2">
         <div class="flex justify-between">
-            <div class="my-2 max-w-[620px]">
-                Co-Founder and CEO at Duggup - Social network for people in tech. Ex-Amazon Head of Engineering. I write hot-takes on building a business, shipping delightful products and accelerating product and career growth.
+            <div class="my-2 2xl:max-w-[620px]">
+                {userData.about}
             </div>
             
             <div class="my-2 ml-10">
                 <div class="flex justify-end">
-                    <img class="right-0" src="/duggupLogo.svg" alt="Link-external" width="36" height="36">
+                    <img class="right-0" src={userData.currentCompanyIcon} alt="Link-external" width="36" height="36">
                 </div>
                 <div class="flex flex-col text-end">
-                    <span class="text-[16px] text-[#000000] font-bold">Duggup</span>
-                    <span class="text-[12px] text-[#7A9299] font-normal">Co-Founder and CEO</span>
+                    <span class="text-[16px] text-[#000000] font-bold">{userData.currentCompany}</span>
+                    <span class="text-[12px] text-[#7A9299] font-normal">{userData.currentPosition}</span>
                 </div>
             </div>
         </div>
         <div class="flex justify-between mt-5">
-            <Button variant="shadowButton" class="h-8 ml-3"> Follow</Button>
+            <Button variant="shadowButton" class="h-8 ml-3">Follow</Button>
             <div class="flex">
-                <a href="https://www.krishnakiran.com/" class="flex" target="_blank">
+                <a href={userData.websiteLink} class="flex" target="_blank">
                     <span class="text-[#4D4D4D] text-sm font-normal">My Website</span> 
                     <img class="mx-1 mb-2" src="/link-external.svg" alt="Link-external" width="12" height="12">
                 </a>
